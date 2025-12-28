62ecfe9f08330f067c0950911cc6ae76f3772167
Shreyash Gawande <shreyashmgawande@gmail.com>
2025-12-29 01:04:54 +0530

    Add files via upload

---
+++ frameworks/spring/JDBC_Template/Book.java
@@ -0,0 +1,41 @@
+package mypack;
+
+public class Book {
+	
+		private int bid;
+		private String bname;
+		private int bprice;
+		public int getBid() {
+			return bid;
+		}
+		public void setBid(int bid) {
+			this.bid = bid;
+		}
+		public String getBname() {
+			return bname;
+		}
+		public void setBname(String bname) {
+			this.bname = bname;
+		}
+		public int getBprice() {
+			return bprice;
+		}
+		public void setBprice(int bprice) {
+			this.bprice = bprice;
+		}
+		
+		public Book() {}
+		
+		public Book(int bid, String bname, int bprice) {
+			super();
+			this.bid = bid;
+			this.bname = bname;
+			this.bprice = bprice;
+		}
+		@Override
+		public String toString() {
+			return "bid = " + bid + ", bname = " + bname + ", bprice = " + bprice ;
+		}
+		
+		
+}
